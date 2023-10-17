1596604237d29a80af7803eb88dc26f1034ddd3b
yarik1025 <yaroslavkurzenkov@gmail.com>
2023-10-18 00:37:32 +0300

    add coursera and irem

---
+++ irem/script.js
@@ -0,0 +1,15 @@
+// let firstName = 'John';
+// let lastName = 'Adams';
+// let result = `${firstName} ${lastName}`;
+
+// console.log(result);
+// firstName = 'Anna';
+// lastName ='Karenina';
+// result = `${firstName} ${lastName}`;
+
+// console.log(result);
+
+let a;
+let c = 9;
+
+console.log(typeof(c))
